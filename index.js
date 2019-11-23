4392af6e102b84bfd066ba39fdd91db58b52349d
stevelagarto <stevenzgz@gmail.com>
2019-11-23 16:47:53 +0100

    express server working

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const clientPath = path.join(__dirname, './client/chat-client-exercise-master/app/REMOVE/');
 const router = require('./router');
-const bodyparser = require('./bodyParser');
+const bodyparser = require('body-parser');
 
-// const server = http.createServer((req, res) => {
-//   bodyparser(req, res, router);
-// });
+
+app.use(express.static(clientPath));
+app.use(bodyparser.json());
+app.use(router);
+
+
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`); // eslint-disable-line no-console
 });
+
+module.exports = app;
--- router.js
+++ router.js
@@ -1,48 +1,21 @@
 const path = require('path');
+const express = require('express');
 const fs = require('fs');
 const db = require('./db');
-const clientPath = path.join(__dirname, '/../../client');
-const mimeTypes = {
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.css': 'text/css',
-  '.json': 'application/json',
-  '.png': 'image/png',
-  '.jpg': 'image/jpg',
-  '.gif': 'image/gif',
-  '.svg': 'image/svg+xml',
-  '.wav': 'audio/wav',
-  '.mp4': 'video/mp4',
-  '.woff': 'application/font-woff',
-  '.ttf': 'application/font-ttf',
-  '.eot': 'application/vnd.ms-fontobject',
-  '.otf': 'application/font-otf',
-  '.wasm': 'application/wasm'
-};
+const router = require('express').Router();
 
-const router = function (req, res) {
-  if (req.url === '/messages' && req.method === 'POST') {
-    db.msgs.push(req.body);
-    res.writeHead(201);
-    res.end();
-  } else if (req.url === '/messages' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-type': mimeTypes['.json'] });
-    res.end(JSON.stringify(db.msgs));
-  } else {
-    // Static files
-    if (req.url === '/') req.url = '/index.html';
-    const extName = path.extname(req.url);
-    const contentType = mimeTypes[extName] || 'application/octet-stream';
-    fs.readFile(clientPath + req.url, (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end();
-      } else {
-        res.writeHead(200, { 'Content-type': contentType });
-        res.end(data);
-      }
-    });
-  }
-};
+router.get('/message', (req, res) => {
+  res.status(200);
+  res.send(JSON.stringify(db.msgs));
+});
+
+router.post('/message', (req, res) => {
+  console.log(req.body);
+  db.msgs.push(req.body);
+  res.status(201);
+  res.send(req.body)
+});
+
+
 
 module.exports = router;
